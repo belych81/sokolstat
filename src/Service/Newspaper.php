48dea96b4cf39f2f4cd1767523bc4a4514934b8c
Alex <belych81@mail.ru>
2021-11-26 21:27:18 +0300

    Edit eurocup link on calendar

--- src/Service/Newspaper.php
+++ src/Service/Newspaper.php
@@ -16,7 +16,7 @@ class Newspaper
   private $em;
   private $props;
   private $functions;
-  private $newspaperDate = '-37 days';
+  private $newspaperDate = '-7 days';
 
   public function __construct(EntityManagerInterface $em, Props $props, Functions $functions)
   {
