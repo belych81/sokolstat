12389404dfac0899bcf3fbe70934ef18fd7adcd0
Alexey <belych81@mail.ru>
2022-11-29 21:37:58 +0300

    styling_thumb_emblem

--- src/Controller/MundialController.php
+++ src/Controller/MundialController.php
@@ -103,7 +103,7 @@ class MundialController extends AbstractController
           ->getCountriesBySeason($year, $turnir);
         foreach($countries as &$country){
           if($country['image']){
-            $country['image'] = $resize->ResizeImageGet($country['image'], ['width' => 70, 'height' => 70]);
+            $country['image'] = $resize->ResizeImageGet($country['image'], ['width' => 80, 'height' => 80]);
           }
         }
         $seasons = $this->getDoctrine()->getRepository(Mundial::class)
