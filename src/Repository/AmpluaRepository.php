607d0037328fc20b0da6865f88956558efc3b9ba
Alexey <belych81@mail.ru>
2023-12-09 21:48:46 +0300

    managerregistry amplua

--- src/Repository/AmpluaRepository.php
+++ src/Repository/AmpluaRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Amplua;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Amplua|null find($id, $lockMode = null, $lockVersion = null)
