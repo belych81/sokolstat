10d79a9ea2e46a2e12f13fbba2fc556ee3ab40eb
Alexey <belych81@mail.ru>
2023-12-10 21:13:50 +0300

    easy admin 3

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Game;
+use App\Entity\Player;
+use App\Entity\City;
+use App\Entity\Country;
+use App\Entity\Referee;
+use App\Entity\Team;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig');
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Sokolstat');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+
+            //MenuItem::section('Blog'),
+            MenuItem::linkToCrud('Game', 'fa fa-tags', Game::class),
+            MenuItem::linkToCrud('Player', 'fa fa-file-text', Player::class),
+
+            //MenuItem::section('Users'),
+            MenuItem::linkToCrud('City', 'fa fa-comment', City::class),
+            MenuItem::linkToCrud('Country', 'fa fa-user', Country::class),
+        ];
+        //yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
---
+++ src/Controller/Admin/GameCrudController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Game;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+
+class GameCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Game::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            Field::new('id'),
+            AssociationField::new('turnir'),
+            Field::new('tour'),
+            Field::new('data'),
+            AssociationField::new('team'),
+            AssociationField::new('team2'),
+            Field::new('score'),
+            Field::new('goal1'),
+            Field::new('goal2'),
+            Field::new('bomb'),
+        ];
+    }
+    
+}
