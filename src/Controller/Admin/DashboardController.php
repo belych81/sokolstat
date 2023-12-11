5d3bbe6b2360872ba72b052ccaf6d9a7a63d3d00
Alexey <belych81@mail.ru>
2023-12-11 20:55:55 +0300

    easy admin 3

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,11 @@ use App\Entity\City;
 use App\Entity\Country;
 use App\Entity\Referee;
 use App\Entity\Team;
+use App\Entity\Mundial;
+use App\Entity\Amplua;
+use App\Entity\Stadia;
+use App\Entity\Turnir;
+use App\Entity\Transfer;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -51,11 +56,17 @@ class DashboardController extends AbstractDashboardController
 
             //MenuItem::section('Blog'),
             MenuItem::linkToCrud('Game', 'fa fa-tags', Game::class),
+            MenuItem::linkToCrud('Mundial', 'fa fa-file-text', Player::class),
             MenuItem::linkToCrud('Player', 'fa fa-file-text', Player::class),
 
-            //MenuItem::section('Users'),
             MenuItem::linkToCrud('City', 'fa fa-comment', City::class),
             MenuItem::linkToCrud('Country', 'fa fa-user', Country::class),
+            MenuItem::linkToCrud('Referee', 'fa fa-comment', Referee::class),
+            MenuItem::linkToCrud('Team', 'fa fa-user', Team::class),
+            MenuItem::linkToCrud('Amplua', 'fa fa-comment', Amplua::class),
+            MenuItem::linkToCrud('Stadia', 'fa fa-user', Stadia::class),
+            MenuItem::linkToCrud('Turnir', 'fa fa-comment', Turnir::class),
+            MenuItem::linkToCrud('Transfer', 'fa fa-user', Transfer::class),
         ];
         //yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
