ffd7a747e848e2c26225df9bfdbd195f3ee8ae25
Alexey <belych81@mail.ru>
2024-01-18 09:22:59 +0300

    mundial admin

--- src/Controller/Admin/MundialCrudController.php
+++ src/Controller/Admin/MundialCrudController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Mundial;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +18,32 @@ class MundialCrudController extends AbstractCrudController
         return Mundial::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            Field::new('id')->hideOnForm(),
+            AssociationField::new('turnir'),
+            AssociationField::new('season'),
+            Field::new('data'),
+            AssociationField::new('country'),
+            AssociationField::new('country2'),
+            Field::new('score'),
+            Field::new('goal'),
+            Field::new('game')->onlyOnForms(),
+            Field::new('game2')->onlyOnForms(),
+            Field::new('penalty')->onlyOnForms(),
+            AssociationField::new('city')->onlyOnForms(),
+            AssociationField::new('referee')->onlyOnForms()
         ];
     }
-    */
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('season')
+            ->add('turnir')
+            ->add('country')
+            ->add('country2')
+        ;
+    }
 }
