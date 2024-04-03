cb5d398b1056188cbd0087718357b6aad8633b3a
Alexey <belych81@mail.ru>
2024-04-03 09:54:23 +0300

    admin player fields

--- src/Controller/Admin/PlayerCrudController.php
+++ src/Controller/Admin/PlayerCrudController.php
@@ -5,8 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\Player;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class PlayerCrudController extends AbstractCrudController
 {
@@ -15,14 +19,45 @@ class PlayerCrudController extends AbstractCrudController
         return Player::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            Field::new('id')->hideOnForm(),
+            AssociationField::new('country'),
+            Field::new('name'),
+            AssociationField::new('amplua')->onlyOnForms(),
+            Field::new('born'),
+            Field::new('game'),
+            Field::new('goal'),
+            Field::new('lch_game'),
+            Field::new('lch_goal'),
+            Field::new('cup')->onlyOnForms(),
+            Field::new('supercup')->onlyOnForms(),
+            Field::new('eurocup')->onlyOnForms(),
+            Field::new('sbornie')->onlyOnForms(),
+            Field::new('sum'),
+            Field::new('translit')->onlyOnForms(),
+            Field::new('image')->onlyOnForms(),
+            Field::new('insertdate')->onlyOnForms(),
+            Field::new('viewed'),
         ];
     }
-    */
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('country')
+            ->add('name')
+            ->add('amplua')
+        ;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return parent::configureCrud($crud)
+            ->setDefaultSort([
+                'sum' => 'DESC',
+                'id' => 'ASC',
+            ]);
+    }
 }
