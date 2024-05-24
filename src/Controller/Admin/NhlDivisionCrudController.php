381b9c31ea61f5bf82f4d297708f8f81d7c42d2e
Alexey <belych81@mail.ru>
2024-05-24 12:39:41 +0300

    division admin

---
+++ src/Controller/Admin/NhlDivisionCrudController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\NhlDivision;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
+class NhlDivisionCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return NhlDivision::class;
+    }
+    
+}
